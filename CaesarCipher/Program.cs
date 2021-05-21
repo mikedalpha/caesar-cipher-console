3f9b3b9a43fd4493a0bd7a9933bb4228fedf7f44
Michael Athanasoglou <mikedalpha@gmail.com>
2021-05-21 11:57:53 +0300

    Init commit

---
+++ CaesarCipher/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace CaesarCipher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Input: ");
+            string input = Console.ReadLine();
+            Console.WriteLine("Key: ");
+            int key = int.Parse(Console.ReadLine());
+            char[] arr = input.ToCharArray();
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if (arr[i] >= 65 && arr[i] <= 90)
+                {
+                    arr[i] += (char)key;
+                    if (arr[i] > 90)
+                    {
+                        arr[i] %= (char)90;
+                        arr[i] += (char)64;
+                    }
+                }
+                else if (arr[i] >= 97 && arr[i] <= 122)
+                {
+                    arr[i] += (char)key;
+                    if (arr[i] > 122)
+                    {
+                        arr[i] %= (char)122;
+                        arr[i] += (char)96;
+                    }
+                }
+                Console.Write(arr[i]);
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Caesar Cipher
+
+This Console app was produced using *.NET 5*, by following the [HackerRank](https://www.hackerrank.com/) practice exercises.
+
+It is my take on the popular **Caesar Cipher** exercise.
+
+Upon startup, the user is prompted to input a string. Afterwards, the user provides the key by which the string's characters are shifted forward.
+Numbers, spaces and special characters are *not* influenced. Upon reaching the end of the alphabet, the cipher loops to the beggining of the alphabet.
